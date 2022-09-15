9fbec3f879a41140a7686a06b45e28d0d01070ca
Edward Tanguay (windows) <edward@tanguay.info>
2022-09-15 15:14:05 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,42 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 const _intitialItem = {
-  title: "the test",
-  materials: ['aaa', 'bbb', 'ccc', 'ddd', 'eee']
+	title: 'the test',
+	materials: ['aaa', 'bbb', 'ccc', 'ddd', 'eee']
 };
 
 function App() {
-  const [item, setItem] = useState(_intitialItem);
+	const [item, setItem] = useState(_intitialItem);
 
-  const handleMaterialChange = (currentItem, e, i) => {
-    const newValue = e.target.value;
-    const _item = {
-      ...currentItem, materials: ['sfdjk']
-    };
-    setItem(_item);
-  }
+	const handleMaterialChange = (currentItem, e, i) => {
+		const newValue = e.target.value;
+		const _item = { ...currentItem };
+    _item.materials[i] = newValue;
+		setItem(_item);
+	};
 
-  return (
-    <div className="App">
-    <h1>Test Array edit</h1>
+	return (
+		<div className="App">
+			<h1>Test Array edit</h1>
 
-      {item.materials.map((material, i) => {
-        return (
-          <div key={i}><input value={item.materials[i]} onChange={ (e) => handleMaterialChange(item, e,i)} /></div>
-        )
-      })}
+			{item.materials.map((material, i) => {
+				return (
+					<div key={i}>
+						<input
+							value={item.materials[i]}
+							onChange={(e) => handleMaterialChange(item, e, i)}
+						/>
+					</div>
+				);
+			})}
 
-      <hr/>
-      {item.materials.map((material, i) => {
-        return (
-          <div key={i}>{material}</div>
-        )
-      })}
-
-
-    </div>
-  )
+			<hr />
+			{item.materials.map((material, i) => {
+				return <div key={i}>{material}</div>;
+			})}
+		</div>
+	);
 }
 
-export default App
+export default App;
